9a613dbd96dd695d2f433f21424d758a54099767
Alexsandro Jose <167003308+Alexsandro793@users.noreply.github.com>
2025-11-25 21:30:50 -0300

    Update GamesRentalApplicationTests.java

--- src/test/java/com/gamesrental/GamesRentalApplicationTests.java
+++ src/test/java/com/gamesrental/GamesRentalApplicationTests.java
@@ -3,7 +3,7 @@ package com.gamesrental;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class GamesRentalApplicationTests {
 
 	@Test
